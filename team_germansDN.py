b313f28e992337fdfebb28bf1ca68e5bbec089b0
David Nelson <nelsond@ledyard.net>
2022-05-12 10:06:36 -0400

    what have you done

--- team_germansDN.py
+++ team_germansDN.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Germans'
+team_name = 'Germany'
 strategy_name = 'Opperation Barbarosa'
 strategy_description = '''
 Collude first round. Collude, except in a round after getting 
